5e4e6a7f7d4e293b4651785cd6020f4eb45347c3
romanusovich1 <romanusovich1@gmail.com>
2023-06-13 10:21:50 +0300

    feat: add env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
+import process from 'process';
+
 const parseEnv = () => {
     // Write your code here 
+    for (const [key, value] of Object.entries(process.env)) {
+        console.log(`RSS_${key}=${value};`);
+    }
 };
 
 parseEnv();
