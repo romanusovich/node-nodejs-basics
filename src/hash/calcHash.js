d0cbcd6577791d3301b29ce829e03a6ed8adbe28
romanusovich1 <romanusovich1@gmail.com>
2023-06-13 11:25:31 +0300

    feat: add calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,13 @@
+import * as fs from 'fs';
+import { createHash } from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+    // Write your code here
+    const filePath = './src/hash/files/fileToCalculateHashFor.txt';
+    fs.readFile(filePath, (err, data) => {
+        if (err) throw err;
+        console.log(createHash('sha256').update(data.toString()).digest('hex'));
+    });
 };
 
 await calculateHash();
