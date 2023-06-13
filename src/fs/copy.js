c2491eed422aa991cbbe3b807c25726071c9ebc3
romanusovich1 <romanusovich1@gmail.com>
2023-06-13 09:27:10 +0300

    feat: add copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,14 @@
+import * as fs from 'fs';
+
 const copy = async () => {
     // Write your code here 
+    const source = './src/fs/files';
+    const destination = './src/fs/files_copy';
+    fs.cp(source, destination, { recursive: true, force: false, errorOnExist: true }, (err) => {
+        if (err !== null &&
+            (err.code === 'ENOENT' ||
+                err.code === 'ERR_FS_CP_EEXIST')) throw Error('FS operation failed');
+    });
 };
 
 await copy();
