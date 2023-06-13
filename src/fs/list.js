265de3d64f85949f2c93778b572040d86b12e6e3
romanusovich1 <romanusovich1@gmail.com>
2023-06-13 10:11:11 +0300

    feat: add list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,13 @@
+import * as fs from 'fs';
+
 const list = async () => {
     // Write your code here 
+    const dirPath = './src/fs/files';
+    fs.readdir(dirPath, (err, files) => {
+        if (err && err.code === 'ENOENT') throw Error('FS operation failed');
+        else if (err) throw err;
+        console.log(files);
+    })
 };
 
 await list();
