afa7762b5273af9fe07d5925ced09af22f7c919e
romanusovich1 <romanusovich1@gmail.com>
2023-06-13 10:13:27 +0300

    feat: add read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,13 @@
+import * as fs from 'fs';
+
 const read = async () => {
     // Write your code here 
+    const filePath = './src/fs/files/fileToRead.txt';
+    fs.readFile(filePath, (err, data) => {
+        if (err && err.code === 'ENOENT') throw Error('FS operation failed');
+        else if (err) throw err;
+        console.log(data.toString());
+    });
 };
 
 await read();
