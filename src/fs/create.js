718e2eff8949a6d3a3bc31edf1196b45d13991c1
romanusovich1 <romanusovich1@gmail.com>
2023-06-13 09:10:43 +0300

    feat: add create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,10 @@
+import { writeFile } from 'fs';
+
 const create = async () => {
     // Write your code here 
+    writeFile('./src/fs/files/fresh.txt', 'I am fresh and young', { flag: 'wx' }, (err) => { 
+        if (err) throw Error('FS operation failed');
+    });
 };
 
 await create();
