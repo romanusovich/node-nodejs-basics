5f1e8917a6233d036c41ee924dd1e0bac761d8ec
romanusovich1 <romanusovich1@gmail.com>
2023-06-13 10:05:15 +0300

    feat: add delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,12 @@
+import * as fs from 'fs';
+
 const remove = async () => {
     // Write your code here 
+    const removePath = './src/fs/files/fileToRemove.txt';
+    fs.unlink(removePath, (err) => {
+        if (err && err.code === 'ENOENT') throw Error('FS operation failed');
+        else if (err) throw err;
+    });
 };
 
 await remove();
