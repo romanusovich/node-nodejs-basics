d7d40b89cccb0f8979e65261c512f27cc2ee89a3
romanusovich1 <romanusovich1@gmail.com>
2023-06-14 09:40:51 +0300

    feat: add write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,12 @@
+import * as fs from 'fs';
+import { stdout, stdin } from 'process';
+
 const write = async () => {
     // Write your code here 
+    stdout.write('Write something\nPress "ctrl + c" to leave\n');
+    const filePath = './files/fileToWrite.txt';
+    const writeStream = fs.createWriteStream(filePath);
+    stdin.on('data', (data) => writeStream.write(data));
 };
 
 await write();
