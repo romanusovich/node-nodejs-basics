a43f2ae04da6c877bb9e4bd46b29c022490c89ed
romanusovich1 <romanusovich1@gmail.com>
2023-06-14 09:34:25 +0300

    feat: add read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,11 @@
+import * as fs from 'fs';
+import { stdout } from 'process';
+
 const read = async () => {
     // Write your code here 
+    const filePath = './files/fileToRead.txt';
+    const readStream = fs.createReadStream(filePath);
+    readStream.on('data', (data) => stdout.write(data));
 };
 
 await read();
