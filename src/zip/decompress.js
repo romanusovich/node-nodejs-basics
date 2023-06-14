af99509942c0eb42e7d258b661ce3033f3754c6b
romanusovich1 <romanusovich1@gmail.com>
2023-06-14 14:32:24 +0300

    feat: add decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,14 @@
+import Zlib from 'zlib';
+import * as fs from 'fs';
+
 const decompress = async () => {
     // Write your code here 
+    const fileSource = './files/archive.gz';
+    const fileTarget = './files/fileToCompress.txt';
+    const readStream = fs.createReadStream(fileSource);
+    const writeStream = fs.createWriteStream(fileTarget);
+    const unzip = Zlib.createUnzip();
+    readStream.pipe(unzip).pipe(writeStream);
 };
 
 await decompress();
