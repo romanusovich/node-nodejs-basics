3f9817140f1243ffd5d599d08ab76f173ede8490
romanusovich1 <romanusovich1@gmail.com>
2023-06-15 12:31:20 +0300

    feat: add cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,9 @@
+import { spawn } from 'child_process';
+
 const spawnChildProcess = async (args) => {
     // Write your code here
+    spawn('node', args, { stdio: ['inherit', 'inherit', 'inherit', 'ipc'] });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ['./files/script.js' ,'someArgument1', 'someArgument2', 'someArgument3']);
