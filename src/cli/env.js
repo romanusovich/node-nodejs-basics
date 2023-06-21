13da30ec973aae8f284b252257bddc3fdd08a560
romanusovich1 <romanusovich1@gmail.com>
2023-06-21 09:48:36 +0300

    fix: edit env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -3,7 +3,7 @@ import process from 'process';
 const parseEnv = () => {
     // Write your code here 
     for (const [key, value] of Object.entries(process.env)) {
-        console.log(`RSS_${key}=${value};`);
+        if (key.startsWith('RSS_')) process.stdout.write(`${key}=${value}; `);
     }
 };
 
