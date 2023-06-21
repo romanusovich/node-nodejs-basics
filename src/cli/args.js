9ee552fe8613d7d1308eb24218c292b5e2b8e7b7
romanusovich1 <romanusovich1@gmail.com>
2023-06-21 09:49:47 +0300

    fix: edit args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -4,7 +4,7 @@ const parseArgs = () => {
     // Write your code here 
     const args = Object.entries(process.argv).slice(2);
     for (let i = 0; i < args.length; i += 2) {
-        console.log(`${args[i][1].slice(2)} is ${args[i + 1][1]},`);
+        process.stdout.write(`${args[i][1].slice(2)} is ${args[i + 1][1]}, `);
     }
 };
 
